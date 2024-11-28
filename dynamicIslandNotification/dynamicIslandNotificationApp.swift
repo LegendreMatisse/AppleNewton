a3b2bc78c7dd312ecd52c4eb655e94e0d1f18b13
Quinten-14 <raymaekersq@gmail.com>
2024-11-28 17:07:56 +0100

    Initial commit

---
+++ dynamicIslandNotification/dynamicIslandNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamicIslandNotificationApp.swift
+//  dynamicIslandNotification
+//
+//  Created by Quinten Raymaekers on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct dynamicIslandNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dynamicIslandNotificationTests/dynamicIslandNotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  dynamicIslandNotificationTests.swift
+//  dynamicIslandNotificationTests
+//
+//  Created by Quinten Raymaekers on 28/11/2024.
+//
+
+import Testing
+@testable import dynamicIslandNotification
+
+struct dynamicIslandNotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
